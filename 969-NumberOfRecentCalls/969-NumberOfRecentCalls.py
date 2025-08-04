a81dc0bb615f783ec277a282e841ed2e67f99a77
Nishal Pattan <nishal.pattan@gmail.com>
2025-08-03 21:02:39 -0700

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.py
@@ -0,0 +1,15 @@
+# Last updated: 8/3/2025, 9:02:39 PM
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = collections.deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue[0] < t-3000:
+            self.queue.popleft()
+        return len(self.queue)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
