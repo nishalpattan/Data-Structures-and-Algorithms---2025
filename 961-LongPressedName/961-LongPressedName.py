16ffcc8d5e410064d0961243324f2f7c1d585363
Nishal Pattan <nishal.pattan@gmail.com>
2025-08-03 21:02:41 -0700

    Create 961-LongPressedName

---
+++ 961-LongPressedName/961-LongPressedName.py
@@ -0,0 +1,10 @@
+# Last updated: 8/3/2025, 9:02:41 PM
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        g1 = [(k, len(list(grp))) for k, grp in itertools.groupby(name)]
+        g2 = [(k, len(list(grp))) for k, grp in itertools.groupby(typed)]
+        if len(g1) != len(g2):
+            return False
+
+        return all(k1 == k2 and v1 <= v2
+                   for (k1,v1), (k2,v2) in zip(g1, g2))
