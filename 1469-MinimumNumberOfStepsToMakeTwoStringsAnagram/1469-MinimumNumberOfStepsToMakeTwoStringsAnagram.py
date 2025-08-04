7389ceaaf7ded2303a74548475d076043517b749
Nishal Pattan <nishal.pattan@gmail.com>
2025-08-03 21:02:02 -0700

    Create 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram

---
+++ 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram.py
@@ -0,0 +1,16 @@
+# Last updated: 8/3/2025, 9:02:02 PM
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        if len(s) != len(t):
+            return 0
+        hash_map = dict()
+        count = 0
+        for char in s:
+            hash_map[char] = hash_map.get(char, 0) + 1
+        
+        for char in t:
+            if hash_map.get(char, 0) > 0:
+                hash_map[char] -= 1
+            else:
+                count += 1
+        return count
