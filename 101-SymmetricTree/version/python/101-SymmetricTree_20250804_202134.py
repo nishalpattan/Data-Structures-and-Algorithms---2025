fb8aef542e17be183451bea658de422cc0c53cc1
Nishal Pattan <nishal.pattan@gmail.com>
2025-08-04 20:21:34 -0700

    Create 101-SymmetricTree

---
+++ 101-SymmetricTree/version/python/101-SymmetricTree_20250804_202134.py
@@ -0,0 +1,24 @@
+# Last updated: 8/4/2025, 8:21:34 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None: return True
+ 
+        def dfs(left, right):
+            if left is None and right is None:
+                return True
+            
+            if left and right is None:
+                return False
+            if right and left is None:
+                return False
+
+            return left.val == right.val and dfs(left.left, right.right) and dfs(left.right, right.left)
+
+        return dfs(root.left, root.right)
+        
