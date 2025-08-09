4950cc4403e17156c127baf9843b614829e2f865
Nishal Pattan <nishal.pattan@gmail.com>
2025-08-08 20:10:05 -0700

    Create 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold

---
+++ 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/version/python/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold_20250808_201005.py
@@ -0,0 +1,19 @@
+# Last updated: 8/8/2025, 8:10:05 PM
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        if len(arr) == 0:
+            return 0
+        _max_avg = 0
+        _sum = 0
+        count = 0
+        start = 0
+        n = len(arr)
+        for end in range(n):
+            _sum += arr[end]
+            curr_avg = _sum // (end - start + 1)
+            if end - start + 1 == k:
+                if curr_avg >= threshold:
+                    count += 1
+                _sum -= arr[start]
+                start += 1
+        return count 
